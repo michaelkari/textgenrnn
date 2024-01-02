44d5b7c1a68f6aad861181ef14492f9d1e5b4c03
Michael Karimizadeh <michaelnjkari@gmail.com>
2024-01-01 23:29:16 -0500

    Update model.py

--- textgenrnn/model.py
+++ textgenrnn/model.py
@@ -10,7 +10,7 @@ from .AttentionWeightedAverage import AttentionWeightedAverage
 def textgenrnn_model(num_classes, cfg, context_size=None,
                      weights_path=None,
                      dropout=0.0,
-                     optimizer=Adam(lr=4e-3)):
+                     optimizer=Adam(learning_rate=4e-3)):
     '''
     Builds the model architecture for textgenrnn and
     loads the specified weights for the model.
