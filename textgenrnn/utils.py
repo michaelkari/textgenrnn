3cb81d3b205e51b6b676ab5c960cae616c84c512
Michael Karimizadeh <50850314+michaelkari@users.noreply.github.com>
2025-04-02 19:33:21 -0400

    change weight extensions for Keras

--- textgenrnn/utils.py
+++ textgenrnn/utils.py
@@ -302,7 +302,7 @@ class save_model_weights(Callback):
         if self.save_epochs > 0 and (epoch+1) % self.save_epochs == 0 and self.num_epochs != (epoch+1):
             print("Saving Model Weights — Epoch #{}".format(epoch+1))
             self.textgenrnn.model.save_weights(
-                "{}_weights_epoch_{}.hdf5".format(self.weights_name, epoch+1))
+                "{}_weights_epoch_{}.weights.h5".format(self.weights_name, epoch+1))
         else:
             self.textgenrnn.model.save_weights(
-                "{}_weights.hdf5".format(self.weights_name))
+                "{}.weights.h5".format(self.weights_name))
